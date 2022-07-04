36af89752673497c973ab73be484e51c289dcbf3
unknown <tizianamalko@gmail.com>
2022-07-04 18:40:14 +0200

    messagge

---
+++ script.js
@@ -0,0 +1,23 @@
+//Print odds 1-20
+//Using a loop write code that will console.log all of the odd values from 1 up to 20.
+for (var i = 1; i < 20; i++){
+    if ([i] % 2 !== 0){
+        console.log(i);
+    }
+}
+//Decreasing Multiples of 3
+//Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+for (var i=100; i >-1; i--){
+    if ([i] % 3 == 0){
+        console.log(i);
+    }
+}
+
+//Print the sequence
+//Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+for (var i=100; i >-1; i--){
+    if ([i] % 3 == 0){
+        console.log(i);
+    }
+}
